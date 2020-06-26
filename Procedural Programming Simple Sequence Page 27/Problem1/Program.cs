be148bd3d0f403bd30f9299910906cafbe7af4b7
bibhutighimire <ghimirebibhuti@gmail.com>
2020-06-26 01:24:29 -0600

    #Problem 1 - cube of a number

---
+++ Procedural Programming Simple Sequence Page 27/Problem1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //asking user to enter a number
+            Console.WriteLine("Enter a number and i will give u cube of it");
+
+            //Reads number entered by user
+            int userNumber = int.Parse(Console.ReadLine());
+
+            //Putting the formula
+            int cubeOfNumber=userNumber*userNumber*userNumber;
+
+            //Displaying the result
+            Console.WriteLine("The cube of the number is : {0}",cubeOfNumber);
+        }
+    }
+}
