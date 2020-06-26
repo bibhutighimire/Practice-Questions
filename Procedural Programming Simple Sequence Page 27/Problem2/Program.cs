2208aa56091b5454a76768e8e885a8c61d10ef15
bibhutighimire <ghimirebibhuti@gmail.com>
2020-06-26 01:33:52 -0600

    #prob 2 equal split

--- Procedural Programming Simple Sequence Page 27/Problem2/Program.cs
+++ Procedural Programming Simple Sequence Page 27/Problem2/Program.cs
@@ -6,7 +6,22 @@ namespace problem1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Write a program that will take in a total dollar value (as a double) and a number of diners (as an int) and calculate the amount of the bill that each diner should pay (equal split).
+            Console.WriteLine("Enter the number of total dollar value");
+            //reads the total dollar value and converts it to double
+            double totalBill = Convert.ToDouble(Console.ReadLine());
+
+            //asking the number of diners now
+            Console.WriteLine("Enter the number of diners");
+            //ReadOnlySpan the total number of diners
+            int totalDiners = Convert.ToInt32(Console.ReadLine());
+
+            //using divide formula to get equal split
+            double equalSplit=(double)totalBill/totalDiners;
+
+            Console.WriteLine("Each diner should pay ${0}",equalSplit);
+
+
         }
     }
 }
