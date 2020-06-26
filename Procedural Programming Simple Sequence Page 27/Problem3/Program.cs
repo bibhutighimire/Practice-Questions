33266f15bf19ae7ce9aa9f1f6aa9c5105a1774fd
bibhutighimire <ghimirebibhuti@gmail.com>
2020-06-26 01:49:32 -0600

    #problem 3 - finding the year of birth

--- Procedural Programming Simple Sequence Page 27/Problem3/Program.cs
+++ Procedural Programming Simple Sequence Page 27/Problem3/Program.cs
@@ -6,7 +6,25 @@ namespace problem1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            {
+            //Write a program that will take in a user’s age and calculate their birth year (assuming this year’s birthday has passed).
+
+            Console.WriteLine("How old are you?");
+            //reads the user's age
+            int userAge = Convert.ToInt32(Console.ReadLine());
+
+            //asking the number of diners now
+            Console.WriteLine("Enter the current Year");
+            //reads the current year
+            int currentYear = Convert.ToInt32(Console.ReadLine());
+           
+            int yearOfBirth=currentYear-userAge;
+            
+
+            Console.WriteLine("Your year of birth is {0}",yearOfBirth);
+
+
+        }
         }
     }
 }
