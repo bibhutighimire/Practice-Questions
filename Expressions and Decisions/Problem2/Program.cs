c46f51fc40aebb73c444b5f569737168d475e70e
bibhutighimire <ghimirebibhuti@gmail.com>
2020-06-25 21:59:23 -0600

    Checking Even / Odd number Problem completed

--- Expressions and Decisions/Problem2/Program.cs
+++ Expressions and Decisions/Problem2/Program.cs
@@ -6,7 +6,28 @@ namespace problem1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //@lINK https://www.dotnetperls.com/odd
+
+            //Asking user to enter an integer
+
+            Console.WriteLine("ENTER AN INTEGER AND I WILL TELL YOU IF IT IS EVEN OR ODD NUMBER");
+
+            //Reading the input
+            int userNumber= int.Parse(Console.ReadLine());
+
+            //Using the logic that if remainder is 0 then it's even number 
+            //@lINK https://www.dotnetperls.com/odd
+            if(userNumber%2==0)
+            {
+             Console.WriteLine("You entered even number");
+            }
+
+            //Using the logic that if remainder is not 0 then it's odd number  
+            // userNumber%2=!=0
+            else
+            {
+             Console.WriteLine("You entered odd number");
+            }
         }
     }
 }
