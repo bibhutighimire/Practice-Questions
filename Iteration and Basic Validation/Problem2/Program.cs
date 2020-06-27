cb101d83f41771508267b06020fd8ab5de19774c
bibhutighimire <ghimirebibhuti@gmail.com>
2020-06-27 01:30:00 -0600

    #problem 2- do while loop

--- Iteration and Basic Validation/Problem2/Program.cs
+++ Iteration and Basic Validation/Problem2/Program.cs
@@ -6,7 +6,22 @@ namespace Problem1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Write a program that will say “Hello”, and wait for input from the user. It will continue saying “Hello” until the user enters “stop” (case insensitive). Use a do-while loop.
+
+            Console.WriteLine("Hey User! Type Anything");
+            string userInput;
+
+            do
+            {   
+                userInput=Console.ReadLine();
+                
+                //Console.WriteLine("Hey User! Type Anything");
+            }
+            while (userInput!="stop");
+            
+                Console.WriteLine("Dear User, you entered stop so your while loop will terminate");
+            
+            
         }
     }
 }
