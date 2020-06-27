825bbdc28cbaa6b42c75e49af55896d27af662a3
bibhutighimire <ghimirebibhuti@gmail.com>
2020-06-27 00:08:54 -0600

    commiting practice problems

--- Procedural Programming Simple Sequence Page 27/Problem4/Program.cs
+++ Procedural Programming Simple Sequence Page 27/Problem4/Program.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿// using System;
 
-namespace problem1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+// namespace problem1
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             //Write a program that will take in a 4 digit integer and calculate the sum of its digits without ever converting the integer to a string.
+
+//             Console.WriteLine("Insert 4 digit number");
+
+//             int fourDigitNumber= Convert.ToInt32(Console.ReadLine());
+            
+//         }
+//     }
+// }
+            
