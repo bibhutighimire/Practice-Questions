f25d14ab8a70ecc9ade9a98cdbe4dff8f2646848
bibhutighimire <ghimirebibhuti@gmail.com>
2020-06-28 13:21:11 -0600

    commited only problem 1

--- Iteration and Basic Validation/Problem3/Program.cs
+++ Iteration and Basic Validation/Problem3/Program.cs
@@ -6,7 +6,37 @@ namespace Problem1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Write a program that will count from 1 to 100, replacing every number divisible by 3 with “Fizz”, every number divisible by 5 with “Buzz”, 
+            //and every number divisible by both 3 and 5 with “FizzBuzz”. Try to make it as maintainable as possible, allowing more words and numbers to be easily added.
+
+            int[] number=new int[100];
+                for(int i=1;i<=100;i++)
+                {
+                     if(i%3 == 0 && i%5 == 0)
+                    {
+                        Console.WriteLine("FizzBuzz");
+                        
+                    }
+                    else if(i%3 == 0 ) 
+                    {
+                        Console.WriteLine("Fizz");
+                    }
+                   
+                    else if(i%5 == 0 )
+                    {
+                        Console.WriteLine("Buzz");
+                    }
+                    
+                    else{
+                        Console.WriteLine(i);
+                    }
+
+                }
+           
+                    
+                
+            
+            
         }
     }
 }
--- Iteration and Basic Validation/Problem4/Program.cs
+++ Iteration and Basic Validation/Problem4/Program.cs
@@ -6,7 +6,45 @@ namespace Problem1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Write a program that will prompt for up to 20 marks, until the user enters all 20 or enters -1. 
+            //Ensure that the user enters at least 1 mark. When done, output the average, highest and lowest marks from the “class”. 
+            //Once finished, ask if they would like to start again. 
+            //If they enter any integer that is not between -1 and 100 inclusive, prompt again and disregard the last input.
+            //int[] userMarks;
+
+            //@Link }
+            //https://www.w3resource.com/csharp-exercises/array/csharp-array-exercise-18.php
+
+            Console.WriteLine("Hi User! Please enter upto 5 marks here and i will give you average, highest and lowest marks");
+
+            int[] number = new int[5];
+            int maxValue=number[0];
+            int minValue=number[1];
+            //double averageValue=0;
+            for(int i=0;i<5;i++)
+            {
+                while(number[i] != -1)
+                {
+                  Console.WriteLine("Enter again");
+                  number[i]= Convert.ToInt32(Console.ReadLine());   
+                  if(number[i]>maxValue) 
+                  {
+                      maxValue=number[i];
+                  }  
+
+                  else if(number[i]<minValue) 
+                  {
+                      minValue=number[i];
+                  } 
+                  else
+                  {
+
+                  }               
+                  
+                }
+                Console.WriteLine("The maximun value you entered is {0} and minimun value you entered is {1}",maxValue,minValue);
+            }
+             
         }
     }
 }
